823187f3d69b09208da5d65088f2b8337c6c7360
sanatolp <sergeyp.sp@yandex.ru>
2024-07-25 10:23:14 +0300

    Item Selection Menu

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+import json
 from kivymd.app import MDApp
 from kivy.core.window import Window
 from kivy.lang import Builder
+from kivymd.uix.menu import MDDropdownMenu
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.properties import StringProperty
 
 
 Window.size = (320, 600)
+
+dict_translation_lesson = {"Математика": "mathematics", "Русский язык": "russian", "Физика": "physics"}
 
 
 class MainWindow(Screen):
@@ -24,7 +28,32 @@ class MainWindow(Screen):
 
                 
 class SecondWindow(Screen):
-    pass
+    theme = StringProperty()
+
+    def callback(self, button):
+        menu_items = [
+            {
+                "text": item,
+                "on_release": lambda x = item: self.menu_callback(x),
+            } for item in dict_translation_lesson
+        ]
+    
+        self.menu = MDDropdownMenu(items=menu_items)
+        self.menu.caller = button
+        self.menu.open()
+
+
+    def menu_callback(self, text_item):
+        data = {}
+
+        with open('tests.json') as tests:
+            data = json.load(tests)
+
+        lesson = dict_translation_lesson.get(text_item)
+        self.theme = lesson
+        print(data["lessons"][lesson])
+            
+        self.menu.dismiss()
 
 
 class WindowManager(ScreenManager):
@@ -39,6 +68,8 @@ class VPRApp(MDApp):
         self.theme_cls_accent_hue = "400"
 
         self.root = Builder.load_file("main.kv")
+
+        self.second_window = SecondWindow()
     
 
 if __name__ == "__main__":
